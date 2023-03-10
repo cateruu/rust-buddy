a0e74c49f54997c6adf91ee9e9ac330ac6ca0951
cateruu <cater981pl@gmail.com>
2023-03-10 20:21:02 +0100

    site header

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from 'SignUp.module.scss';
+import Navbar from '../../components/Navbar/Navbar';
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default SignUpPage;
